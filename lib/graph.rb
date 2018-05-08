f64cf83b53d9694e6998cc89f4bb136ceed48b05
Americo <ame.zuz@gmail.com>
2018-05-08 11:01:28 -0700

    Create Edge class

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Vertex
   attr_accessor :out_edges, :in_edges, :value
 
@@ -9,14 +11,21 @@ class Vertex
 end
 
 class Edge
+  attr_accessor :from_vertex, :to_vertex, :cost
+
   def initialize(from_vertex, to_vertex, cost = 1)
     @from_vertex = from_vertex
     @to_vertex = to_vertex
     @cost = cost
-
+    to_vertex.in_edges.push(self)
+    from_vertex.out_edges.push(self)
   end
 
   def destroy!
+    to_vertex.in_edges.delete(self)
+    from_vertex.out_edges.delete(self)
 
+    self.from_vertex = nil
+    self.to_vertex = nil
   end
 end
