3c786fb714dff2bdd975908a55c596e4639075cf
Americo <ame.zuz@gmail.com>
2018-05-08 10:35:59 -0700

    Create vertex class

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,11 +1,18 @@
 class Vertex
-  def initialize(value)
+  attr_accessor :out_edges, :in_edges, :value
 
+  def initialize(value)
+    @value = value
+    @out_edges = []
+    @in_edges = []
   end
 end
 
 class Edge
   def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
 
   end
 
